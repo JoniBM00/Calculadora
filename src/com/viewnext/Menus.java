c3629c2a76e5087510460622aca47509ee576803
Jonathan Becerro Martin <jonibm00@gmail.com>
2024-05-02 13:25:10 +0200

    Probando primer commit

---
+++ src/com/viewnext/Menus.java
@@ -0,0 +1,17 @@
+package com.viewnext;
+
+public class Menus {
+
+	public static void mostrarMenuPrincipal() {
+		
+		System.out.println("------------------");
+		System.out.println("-------Menu-------");
+		System.out.println("------------------");
+		System.out.println("1- Sumar.");
+		System.out.println("2- Restar.");
+		System.out.println("3- Multiplicar.");
+		System.out.println("4- Dividir.");
+		System.out.println("5- Salir.");
+	}
+	
+}
---
+++ src/com/viewnext/Util.java
@@ -0,0 +1,13 @@
+package com.viewnext;
+
+public class Util {
+
+	public static double sumar(double num1, double num2) {
+		return num1+num2;
+	}
+	
+	public static double restar(double num1, double num2) {
+		return num1-num2;
+	}
+	
+}
