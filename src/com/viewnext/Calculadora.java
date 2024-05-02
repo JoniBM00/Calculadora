6ff3bf6a28e10d951b46a42b436893437d613da8
Jonathan Becerro Martin <jonibm00@gmail.com>
2024-05-02 16:26:28 +0200

    Calculadora hecha

--- src/com/viewnext/Calculadora.java
+++ src/com/viewnext/Calculadora.java
@@ -1,23 +1,76 @@
 package com.viewnext;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Calculadora {
 
-	
-	public static int pedirNum(String mens) {
-		int num;
+	static final String PRIMER_NUMERO = "Introduzca el primer número: ";
+	static final String SEGUNDO_NUMERO = "Introduzca el segundo número: ";
+
+	static Logger logger = Logger.getLogger(Calculadora.class.getName());
+
+	public static double pedirDoble(String mens) throws InputMismatchException {
+		double num;
 		Scanner teclado = new Scanner(System.in);
-		System.out.println(mens);
-		num = teclado.nextInt();
+
+		logger.info(mens);
+		num = teclado.nextDouble();
+
 		return num;
 	}
-	
-	public static void main(String[] args) {
-		
-		Menus.mostrarMenuPrincipal();
-		
-		
+
+	public static int pedirNum(String mens) throws InputMismatchException {
+		int num;
+		Scanner teclado = new Scanner(System.in);
+
+		logger.info(mens);
+		num = teclado.nextInt();
+
+		return num;
 	}
-	
+
+	public static void main(String[] args) {
+		int key;
+		double resultado = 0;
+
+		do {
+			try {
+				Menus.mostrarMenuPrincipal();
+				key = pedirNum("Introduzca una opción: ");
+
+				switch (key) {
+				case 1:
+					resultado = Util.sumar(pedirDoble(PRIMER_NUMERO), pedirDoble(SEGUNDO_NUMERO));
+					break;
+				case 2:
+					resultado = Util.restar(pedirDoble(PRIMER_NUMERO), pedirDoble(SEGUNDO_NUMERO));
+					break;
+				case 3:
+					resultado = Util.multiplicar(pedirDoble(PRIMER_NUMERO), pedirDoble(SEGUNDO_NUMERO));
+					break;
+				case 4:
+					resultado = Util.dividir(pedirDoble(PRIMER_NUMERO), pedirDoble(SEGUNDO_NUMERO));
+					break;
+				case 5:
+					logger.info("Saliendo.");
+					break;
+
+				default:
+					throw new IllegalArgumentException("Unexpected value: " + key);
+				}
+				if (logger.isLoggable(Level.INFO)) {
+					logger.info("El resultado es: " + resultado);
+				}
+
+			} catch (InputMismatchException e) {
+				logger.warning("Error, no ha introduccido un número.");
+				key = 0;
+			}
+		} while (key != 5);
+
+	}
+
 }
