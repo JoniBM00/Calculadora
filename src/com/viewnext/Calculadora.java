068133ca03266e8136464dc91e4c9be0a178c37e
Jonathan Becerro Martin <jonibm00@gmail.com>
2024-05-02 13:35:23 +0200

    MasterConMenu

--- src/com/viewnext/Calculadora.java
+++ src/com/viewnext/Calculadora.java
@@ -1,9 +1,22 @@
 package com.viewnext;
 
+import java.util.Scanner;
+
 public class Calculadora {
 
 	
+	public static int pedirNum(String mens) {
+		int num;
+		Scanner teclado = new Scanner(System.in);
+		System.out.println(mens);
+		num = teclado.nextInt();
+		return num;
+	}
+	
 	public static void main(String[] args) {
+		
+		Menus.mostrarMenuPrincipal();
+		
 		
 	}
 	
--- src/com/viewnext/Util.java
+++ src/com/viewnext/Util.java
@@ -6,8 +6,32 @@ public class Util {
 		return num1+num2;
 	}
 	
+	public static int sumar(int num1, int num2) {
+		return num1+num2;
+	}
+	
 	public static double restar(double num1, double num2) {
 		return num1-num2;
+	}
+	
+	public static int restar(int num1, int num2) {
+		return num1-num2;
+	}
+	
+	public static double multiplicar(double num1, double num2) {
+		return num1*num2;
+	}
+	
+	public static int multiplicar(int num1, int num2) {
+		return num1*num2;
+	}
+	
+	public static double dividir (double num1, double num2) {
+		return num1/num2;
+	}
+	
+	public static double dividir (int num1, int num2) {
+		return num1/num2;
 	}
 	
 }
